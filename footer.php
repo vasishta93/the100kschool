be975894c699cf8e6a8fb4b44ef19c87eda2eede
Ubuntu <ubuntu@ascian.in>
2021-12-15 01:26:10 +0530

    footer fix

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 
-        <div class="footer-section section"data-bg-image="assets/images/bg/background-pattern-grid-line.png" data-bg-color="#D5ECC2">
+        <div class="footer-section section" style="position: relative;" data-bg-image="assets/images/bg/background-pattern-grid-line.png" data-bg-color="#D5ECC2">
             <div class="container">
 
                 <!-- Footer Copyright Start -->
@@ -12,4 +12,9 @@
                 <!-- Footer Copyright End -->
 
             </div>
+			<div class="section-bottom-shape">
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="none" height="100">
+					<path class="elementor-shape-fill" d="M 0 0 L0 100 L100 100 L100 0 Q 50 200 0 0"></path>
+				</svg>
+			</div>
         </div>
